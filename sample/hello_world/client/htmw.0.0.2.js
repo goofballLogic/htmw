75fd9a49a302e0bb3e4a4aad1d5e6d06e31deb74
Andrew Stewart Gibson <andrew@commonwealthlabs.co.uk>
2025-12-02 20:37:24 +0000

    tweak

--- sample/hello_world/client/htmw.0.0.2.js
+++ sample/hello_world/client/htmw.0.0.2.js
@@ -1,17 +1,17 @@
 const ops = {
-    value: (el, content) => { if (el) el.value = content; },
+    value: (el, content) => { el.value = content; },
     remove: el => el.remove(),
     _: (el, content) => el.innerHTML += content
 };
-
 document.addEventListener("submit", async function handleSubmit(e) {
 
-    if (!e.target?.hasAttribute("htmw")) return;
+    const form = e.target;
+    if (!form?.hasAttribute("htmw")) return;
     e.preventDefault();
     const data = JSON.stringify([
-        e.target.tagName,
-        e.target.getAttribute("id"),
-        Object.fromEntries(new FormData(e.target).entries())
+        form.tagName,
+        form.getAttribute("id"),
+        Object.fromEntries(new FormData(form).entries())
     ]);
     const resp = await fetch("/htmw", { method: "POST", body: data, headers: { "content-type": "application/json" } });
     if(!resp.ok) throw new Error(`${resp.status} ${resp.statusText}`);
