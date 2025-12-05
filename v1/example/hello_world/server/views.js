05e0e4c918c2ec4f3a35a40ccc1b984f84a57059
Andrew Stewart Gibson <andrew@commonwealthlabs.co.uk>
2025-12-05 14:16:08 +0000

    rm v1

--- v1/example/hello_world/server/views.js
+++
@@ -1,7 +0,0 @@
-export const todoItem = ({ listId, id, name }) => `
-    <form htmw id="complete">
-        <input type="hidden" name="id" value="${id}" />
-        <input type="hidden" name="list_id" value="${listId}" />
-        <button type="submit">${name}</button>
-    </form>
-`;
